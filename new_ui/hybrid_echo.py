9cefb9678c7311f69c575fdb480fc5312dacef40
Moritz Herzog <moritz.herzog@ukdd.de>
2022-02-11 15:29:46 +0100

    --

--- new_ui/hybrid_echo.py
+++ new_ui/hybrid_echo.py
@@ -170,7 +170,7 @@ class MainWindow(MW_Base, MW_Ui):
         self.populate_coordinates_red(coordinates_red)
 
         for i in calibration_rotation_blue:
-            coordinates_message_blue = RoboterOperation.message_assembler(self, calibration_rotation_blue[i])
+            coordinates_message_blue = RoboterOperation.message_assembler(self, i)
             robo_message_blue = RoboterOperation.roboter_message_move(self, network_parameters.tnblue, "C:R:GOTO_POSITION", coordinates_message_blue, "R:C:GOTO_POSITION")
             coordinates_blue = RoboterOperation.message_parser(self, robo_message_blue)
             self.populate_coordinates_blue(coordinates_blue)
