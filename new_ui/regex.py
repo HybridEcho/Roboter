dce97745666e995c546a0cfa62053d43f9c0f752
Julian Kober <julian.kober@tu-dresden.de>
2022-03-04 13:16:17 +0100

    Clean up commit

--- new_ui/regex.py
+++ new_ui/regex.py
@@ -3,14 +3,12 @@ import re
 data_b = b'Welcome to RCX340\r\n\r\nP11=1 -170.916 9.995 32.331 0.000 0.000 1 0 0\r\nR:C:CURRENT_POSITION\r\n'
 data_str = data_b.decode() # convert byte to string
 
-data_str_split = data_str.split('P11')[1] # split str by "P11" .... HARDCODED! .... You aren't gonna need it (YAGNI)
-# print(data_str_split)
+data_str_split = data_str.split('P11')[1] # split str by "P11" 
 
-num_values =  re.findall(r'[-+]?(?:\d*\.\d+|\d+)', data_str_split) # this is the magic.... erzähle ich dir morgen 
+num_values =  re.findall(r'[-+]?(?:\d*\.\d+|\d+)', data_str_split) 
 
-x = num_values[0]
-y = num_values[1]
-z = num_values[2]
-r = num_values[3]
+x = float(num_values[0])
+y = float(num_values[1])
+z = float(num_values[2])
+r = float(num_values[3])
 
-print("x", x, "y", y, "z", z, "r", r)
